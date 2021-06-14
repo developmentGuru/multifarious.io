a28a5679b64cfa78b4e0363a50464c35c0cccecd
developmentGuru <meizeb@rpi.edu>
2021-06-14 16:10:08 -0400

    python commit
    print hello world.
    
    print evens from 0 to 10
    
    implemented with python guard

---
+++ python.py
@@ -0,0 +1,11 @@
+def printHelloWorld():
+    print("Hello World!\n")
+
+def printLoop():
+    for i in range(11):
+        if i % 2 == 0:
+           print(i)
+
+if __name__ == "__main__":
+    printHelloWorld()
+    printLoop()
